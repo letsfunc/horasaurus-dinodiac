10169f42d3b200c1c8c385112fb9a80c2392f3ab
Diana Jean C. Tuquib <deetuquib@users.noreply.github.com>
2021-12-04 10:14:01 -0500

    create new files

---
+++ src/scripts/index.ts
@@ -0,0 +1,16 @@
+$(document).ready(function () {
+    function selectTheme() {
+        // Obtains an array of all <link>
+        // elements.
+        // Select your element using indexing.
+        var theme = document.getElementsByTagName("link")[0];
+
+        // Change the value of href attribute
+        // to change the css sheet.
+        if (theme.getAttribute("href") == "light.css") {
+            theme.setAttribute("href", "dark.css");
+        } else {
+            theme.setAttribute("href", "light.css");
+        }
+    }
+});
